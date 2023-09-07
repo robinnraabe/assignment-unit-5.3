2d02a66719691266cdeaeb0ced1955035cd8e6b1
Robin Raabe <robinnraabe@gmail.com>
2023-09-06 21:00:06 -0500

    added 'showCollection' function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,6 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
-// primesCollection
+// There is no primesCollection?
 let myCollection = [];
 
 function addToCollection (collection, title, artist, yearPublished) {
@@ -15,11 +15,18 @@ function addToCollection (collection, title, artist, yearPublished) {
 
 console.log('Added:', addToCollection(myCollection, 'Galore', 'Oklou', 2020));
 console.log('Added:', addToCollection(myCollection, 'Sea and the Darkness', 'Galileo Galilei', 2016));
-console.log('Added:',  addToCollection(myCollection, 'BBHF1 - Young Man Goes South', 'BBHF', 2020));
-console.log('Added:',  addToCollection(myCollection, 'Bee And The Whales', 'Galileo Galilei', 2023));
-console.log('Added:',  addToCollection(myCollection, 'food house', 'food house', 2020));
-console.log('Added:',  addToCollection(myCollection, 'UFOF', 'Big Thief', 2019));
+console.log('Added:', addToCollection(myCollection, 'BBHF1 - Young Man Goes South', 'BBHF', 2020));
+console.log('Added:', addToCollection(myCollection, 'Bee And The Whales', 'Galileo Galilei', 2023));
+console.log('Added:', addToCollection(myCollection, 'food house', 'food house', 2020));
+console.log('Added:', addToCollection(myCollection, 'UFOF', 'Big Thief', 2019));
 console.log(myCollection);
+
+function showCollection(collection) {
+  for (let object of collection) {
+    console.log(`${object.title} by ${object.artist}, published in ${object.yearPublished}`);
+  }
+}
+showCollection(myCollection);
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
