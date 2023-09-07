dc7b37f6feea8385d2b7d7d6319a3110c57f7ff6
Robin Raabe <robinnraabe@gmail.com>
2023-09-06 20:49:41 -0500

    added 'addToCollection' function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,25 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+// primesCollection
+let myCollection = [];
 
+function addToCollection (collection, title, artist, yearPublished) {
+  let albumToAdd = {
+    title,
+    artist,
+    yearPublished
+  }
+  collection.push(albumToAdd);
+  return albumToAdd;
+}
 
-
-
-
-
-
+console.log('Added:', addToCollection(myCollection, 'Galore', 'Oklou', 2020));
+console.log('Added:', addToCollection(myCollection, 'Sea and the Darkness', 'Galileo Galilei', 2016));
+console.log('Added:',  addToCollection(myCollection, 'BBHF1 - Young Man Goes South', 'BBHF', 2020));
+console.log('Added:',  addToCollection(myCollection, 'Bee And The Whales', 'Galileo Galilei', 2023));
+console.log('Added:',  addToCollection(myCollection, 'food house', 'food house', 2020));
+console.log('Added:',  addToCollection(myCollection, 'UFOF', 'Big Thief', 2019));
+console.log(myCollection);
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
